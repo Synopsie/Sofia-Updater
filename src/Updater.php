ba40d41380325f3db8014f3cbadff0c272a891aa
Dumont-Julien <dumontj357@gmail.com>
2024-08-02 10:38:12 +0200

    CS: fixer

--- src/Updater.php
+++ src/Updater.php
@@ -1,4 +1,20 @@
 <?php
+
+/*
+ *  ____   __   __  _   _    ___    ____    ____    ___   _____
+ * / ___|  \ \ / / | \ | |  / _ \  |  _ \  / ___|  |_ _| | ____|
+ * \___ \   \ V /  |  \| | | | | | | |_) | \___ \   | |  |  _|
+ *  ___) |   | |   | |\  | | |_| | |  __/   ___) |  | |  | |___
+ * |____/    |_|   |_| \_|  \___/  |_|     |____/  |___| |_____|
+ *
+ * Sofia-Updater est une api permettant à un plugin de vérifier si une nouvelle release est disponible.
+ *
+ * @author Synopsie
+ * @link https://neta.arkaniastudios.com/
+ * @version 1.0.0
+ *
+ */
+
 declare(strict_types=1);
 
 namespace sofia;
@@ -7,8 +23,7 @@ use pocketmine\Server;
 use sofia\task\UpdaterAsyncTask;
 
 final class Updater {
-
-    public static function checkUpdate(string $name, string $version, string $owner, string $repo): void {
-        Server::getInstance()->getAsyncPool()->submitTask(new UpdaterAsyncTask($name, $version, $owner, $repo));
-    }
+	public static function checkUpdate(string $name, string $version, string $owner, string $repo) : void {
+		Server::getInstance()->getAsyncPool()->submitTask(new UpdaterAsyncTask($name, $version, $owner, $repo));
+	}
 }
--- src/task/UpdaterAsyncTask.php
+++ src/task/UpdaterAsyncTask.php
@@ -1,4 +1,20 @@
 <?php
+
+/*
+ *  ____   __   __  _   _    ___    ____    ____    ___   _____
+ * / ___|  \ \ / / | \ | |  / _ \  |  _ \  / ___|  |_ _| | ____|
+ * \___ \   \ V /  |  \| | | | | | | |_) | \___ \   | |  |  _|
+ *  ___) |   | |   | |\  | | |_| | |  __/   ___) |  | |  | |___
+ * |____/    |_|   |_| \_|  \___/  |_|     |____/  |___| |_____|
+ *
+ * Sofia-Updater est une api permettant à un plugin de vérifier si une nouvelle release est disponible.
+ *
+ * @author Synopsie
+ * @link https://neta.arkaniastudios.com/
+ * @version 1.0.0
+ *
+ */
+
 declare(strict_types=1);
 
 namespace sofia\task;
@@ -6,58 +22,63 @@ namespace sofia\task;
 use pocketmine\scheduler\AsyncTask;
 use pocketmine\Server;
 use pocketmine\utils\Internet;
+use function json_decode;
+use function sprintf;
+use function str_replace;
+use function version_compare;
+use function vsprintf;
 
 class UpdaterAsyncTask extends AsyncTask {
+	private const GITHUB_RELEASES_URL = "https://api.github.com/repos/%s/%s/releases";
 
-    private const GITHUB_RELEASES_URL = "https://api.github.com/repos/%s/%s/releases";
+	public function __construct(
+		private readonly string $pluginName,
+		private readonly string $pluginVersion,
+		private readonly string $githubOwner,
+		private readonly string $githubRepo
+	) {
+	}
 
-    public function __construct(
-        private readonly string $pluginName,
-        private readonly string $pluginVersion,
-        private readonly string $githubOwner,
-        private readonly string $githubRepo
-    ) {}
+	public function onRun() : void {
+		$url            = sprintf(self::GITHUB_RELEASES_URL, $this->githubOwner, $this->githubRepo);
+		$json           = Internet::getURL($url, 10, ['User-Agent: Sofia-Updater'], $err);
+		$highestVersion = $this->pluginVersion;
+		$artifactUrl    = "";
+		$api            = "";
+		if ($json !== null) {
+			$releases = json_decode($json->getBody(), true);
+			if ($releases !== null) {
+				foreach ($releases as $release) {
+					$version = str_replace("v", "", $release["tag_name"]);
+					if (version_compare($highestVersion, $version, ">=")) {
+						continue;
+					}
+					$highestVersion = $version;
+					$artifactUrl    = $release["html_url"];
+					$api            = $this->pluginName;
+				}
+			}
+		}
 
-    public function onRun(): void {
-        $url = sprintf(self::GITHUB_RELEASES_URL, $this->githubOwner, $this->githubRepo);
-        $json = Internet::getURL($url, 10, ['User-Agent: Sofia-Updater'], $err);
-        $highestVersion = $this->pluginVersion;
-        $artifactUrl = "";
-        $api = "";
-        if ($json !== null) {
-            $releases = json_decode($json->getBody(), true);
-            if ($releases !== null) {
-                foreach ($releases as $release) {
-                    $version = str_replace("v", "", $release["tag_name"]);
-                    if (version_compare($highestVersion, $version, ">=")) {
-                        continue;
-                    }
-                    $highestVersion = $version;
-                    $artifactUrl = $release["html_url"];
-                    $api = $this->pluginName;
-                }
-            }
-        }
+		$this->setResult([$highestVersion, $artifactUrl, $api, $err]);
+	}
 
-        $this->setResult([$highestVersion, $artifactUrl, $api, $err]);
-    }
+	public function onCompletion() : void {
+		$plugin = Server::getInstance()->getPluginManager()->getPlugin($this->pluginName);
+		if ($plugin === null) {
+			return;
+		}
 
-    public function onCompletion(): void {
-        $plugin = Server::getInstance()->getPluginManager()->getPlugin($this->pluginName);
-        if ($plugin === null) {
-            return;
-        }
+		[$highestVersion, $artifactUrl, $api, $err] = $this->getResult();
 
-        [$highestVersion, $artifactUrl, $api, $err] = $this->getResult();
+		if ($err !== null) {
+			$plugin->getLogger()->error("Update notify error: $err");
+			return;
+		}
 
-        if ($err !== null) {
-            $plugin->getLogger()->error("Update notify error: $err");
-            return;
-        }
-
-        if ($highestVersion !== $this->pluginVersion) {
-            $plugin->getLogger()->notice(vsprintf("Version %s has been released for API %s. Download the new release at %s", ['v' . $highestVersion, $api, $artifactUrl]));
-        }
-    }
+		if ($highestVersion !== $this->pluginVersion) {
+			$plugin->getLogger()->notice(vsprintf("Version %s has been released for API %s. Download the new release at %s", ['v' . $highestVersion, $api, $artifactUrl]));
+		}
+	}
 
 }
