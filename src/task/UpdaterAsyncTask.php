2e48fe75f10aff848086e62a0b8f5a80a2bec8c9
Dumont-Julien <dumontj357@gmail.com>
2024-08-06 21:43:43 +0200

    fix crash

--- src/task/UpdaterAsyncTask.php
+++ src/task/UpdaterAsyncTask.php
@@ -20,8 +20,8 @@ declare(strict_types=1);
 namespace sofia\task;
 
 use pocketmine\scheduler\AsyncTask;
-use pocketmine\Server;
 use pocketmine\utils\Internet;
+
 use function is_null;
 use function json_decode;
 use function sprintf;
@@ -30,61 +30,67 @@ use function version_compare;
 use function vsprintf;
 
 class UpdaterAsyncTask extends AsyncTask {
-	private const GITHUB_RELEASES_URL = "https://api.github.com/repos/%s/%s/releases";
+    private const GITHUB_RELEASES_URL = "https://api.github.com/repos/%s/%s/releases";
 
-	public function __construct(
-		private readonly string $pluginName,
-		private readonly string $pluginVersion,
-		private readonly string $githubOwner,
-		private readonly string $githubRepo,
-		private readonly ?string $githubToken
-	) {
-	}
+    public function __construct(
+        private readonly string $pluginName,
+        private readonly string $pluginVersion,
+        private readonly string $githubOwner,
+        private readonly string $githubRepo,
+        private readonly ?string $githubToken
+    ) {
+    }
 
-	public function onRun() : void {
-		$url     = sprintf(self::GITHUB_RELEASES_URL, $this->githubOwner, $this->githubRepo);
-		$headers = ['User-Agent: Sofia-Updater'];
-		if (!is_null($this->githubToken)) {
-			$headers[] = 'Authorization: Bearer ' . $this->githubToken;
-		}
-		$json           = Internet::getURL($url, 10, $headers, $err);
-		$highestVersion = $this->pluginVersion;
-		$artifactUrl    = "";
-		$api            = "";
-		if ($json !== null) {
-			$releases = json_decode($json->getBody(), true);
-			if ($releases !== null) {
-				foreach ($releases as $release) {
-					$version = str_replace("v", "", $release["tag_name"]);
-					if (version_compare($highestVersion, $version, ">=")) {
-						continue;
-					}
-					$highestVersion = $version;
-					$artifactUrl    = $release["html_url"];
-					$api            = $this->pluginName;
-				}
-			}
-		}
+    public function onRun() : void {
+        $url     = sprintf(self::GITHUB_RELEASES_URL, $this->githubOwner, $this->githubRepo);
+        $headers = ['User-Agent: Sofia-Updater'];
+        if (!is_null($this->githubToken)) {
+            $headers[] = 'Authorization: Bearer ' . $this->githubToken;
+        }
+        $response       = Internet::getURL($url, 10, $headers, $err);
+        $highestVersion = $this->pluginVersion;
+        $artifactUrl    = "";
+        $api            = "";
+        $error          = null;
 
-		$this->setResult([$highestVersion, $artifactUrl, $api, $err]);
-	}
+        if ($response !== null) {
+            $status = $response->getCode();
+            if ($status !== 200) {
+                $error = "Failed to fetch releases. HTTP Status Code: $status";
+            } else {
+                $releases = json_decode($response->getBody(), true);
+                if ($releases !== null) {
+                    foreach ($releases as $release) {
+                        $version = str_replace("v", "", $release["tag_name"]);
+                        if (version_compare($highestVersion, $version, ">=")) {
+                            continue;
+                        }
+                        $highestVersion = $version;
+                        $artifactUrl    = $release["html_url"];
+                        $api            = $this->pluginName;
+                    }
+                } else {
+                    $error = "Failed to decode JSON response.";
+                }
+            }
+        } else {
+            $error = $err;
+        }
 
-	public function onCompletion() : void {
-		$plugin = Server::getInstance()->getPluginManager()->getPlugin($this->pluginName);
-		if ($plugin === null) {
-			return;
-		}
+        $this->setResult([$highestVersion, $artifactUrl, $api, $error]);
+    }
 
-		[$highestVersion, $artifactUrl, $api, $err] = $this->getResult();
+    public function onCompletion() : void {
 
-		if ($err !== null) {
-			$plugin->getLogger()->error("Update notify error: $err");
-			return;
-		}
+        [$highestVersion, $artifactUrl, $api, $err] = $this->getResult();
 
-		if ($highestVersion !== $this->pluginVersion) {
-			$plugin->getLogger()->notice(vsprintf("Version %s has been released for API %s. Download the new release at %s", ['v' . $highestVersion, $api, $artifactUrl]));
-		}
-	}
+        if ($err !== null) {
+            echo "§c[Sofia-Updater] Update notify error: $err";
+            return;
+        }
 
+        if ($highestVersion !== $this->pluginVersion) {
+            echo "§b[Sofia-Updater]" . vsprintf("Version %s has been released for API %s. Download the new release at %s", ['v' . $highestVersion, $api, $artifactUrl]);
+        }
+    }
 }
